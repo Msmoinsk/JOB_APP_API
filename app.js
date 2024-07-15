b413fc9c8171fbb3363a281a427028163b76b54c
Msmoinsk <moinsk5365@gmail.com>
2024-07-15 13:41:22 +0530

    security for the server is provided: helmet(), cors(), xss-clean(), express-rate-limit(), and the concept of trusting proxy that is load balancer or the third party server hosting app. for more read the readme

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// advance Load for file
 require('dotenv').config();
 require('express-async-errors');
+
+// Extra security Packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+// -> package name is 'express-rate-limiter'
+const rateLimiter = require('express-rate-limit')
+
+// Express Loading 
 const express = require('express');
 const app = express();
 
@@ -17,8 +27,24 @@ const authRouter = require('./routes/auth')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// extra packages
+// extra packages + Security Packages
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,    // 15 minutes
+    max: 100,   // Limit each Ip to 100 Request per Window
+  })
+)
+/** The Above code is Explained here...... dont get confused
+ * 1. Imagine your Node.js application is hosted on a cloud platform behind a load balancer or a proxy server like Nginx.
+ * 2. Requests from clients reach the load balancer/proxy first, which then forwards them to your Node.js application.
+ * 3. Without app.set('trust proxy', 1);, your application might see all requests coming from the load balancer/proxy's IP address.
+ * 4. By setting app.set('trust proxy', 1);, Express will look at the headers (X-Forwarded-For, etc.) to find out the actual client's IP address as forwarded by the proxy.
+ */
 app.use(express.json());
+app.use(helmet())  // secure http Headers
+app.use(cors())    // allow frontend from any domain or computer to access the API 
+app.use(xss())     // sanatize the req.{body, params, query} from Cross Site Scripting
 
 // routes
 app.use('/api/v1/auth', authRouter)
